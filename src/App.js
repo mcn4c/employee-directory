122b0caaa1afd9813e1ebcaaae75d7392accbcc2
Mary C <mcn4c@virginia.edu>
2021-04-03 09:07:31 -0400

    fixed App.js to pull in TableContainer

--- src/App.js
+++ src/App.js
@@ -1,78 +1,8 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import Table from './components/Table';
-import axios from 'axios';
-import employee from "./employee.json"
+import React from 'react';
+import TableContainer from './components/TableContainer';
 
 function App() {
-
-	const data = employee;
-
-	
-	const columns = useMemo(() => [
-	
-		{
-			Header: "Employee",
-
-			columns: [
-
-		{
-			Header: 'Image',
-			accessor: 'picture.thumbnail'
-		},
-		{
-			Header: 'Name',
-			accessor: [ 'employee.name.last', 'name.first' ]
-		},
-	],
-},
-		{ 
-			Header: "Details",
-		
-		columns: [
-			{
-			Header: 'Age',
-			accessor: 'dob.age'
-		},
-		{
-			Header: 'Contact',
-			accessor: 'email'
-		},
-		{
-			Header: 'Location',
-			accessor: 'location.city'
-		}
-	]
+	return <TableContainer />;
 }
-],
-[]
-);
-	return (
-		<div className="App">
-			<Table columns={columns} data={data} />
-		</div>
-	);
-}
-
-//*************For Cell Styling**************** */
-//Place above App()
-// Custom component to render Genres
-//const Genres = ({ values }) => {
-// Loop through the array and create a badge-like component instead of a comma-separated string
-// return (
-//   <>
-//     {values.map((genre, idx) => {
-//       return (
-//         <span key={idx} className="badge">
-//           {genre}
-//         </span>
-//       );
-//     })}
-//   </>
-// );
-// };
-
-// Cell method will provide the cell value; we pass it to render a custom component
-// place this in useMemo array
-//Cell: ({ cell: { value } }) => <Genres values={value} />
 
 export default App;
